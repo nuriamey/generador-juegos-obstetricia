144e655839bd64bd4a4b625786958a3e2c43c822
nuriamey <nurimeynet1@gmail.com>
2025-04-23 12:45:54 -0300

    Add files via upload

---
+++ src/GeneradorJuegos.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function GeneradorJuegos() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>¡Hola! Esto está funcionando en Vercel 🎉</h1>
+    </div>
+  );
+}
+
+export default GeneradorJuegos;
